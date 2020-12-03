96e131c38794c1a041a9cf74dada54d1ccd7f5d6
Kane Hall <kanehall27@Kanes-Mac.local>
2020-12-02 20:11:03 -0500

    Commit addition of arrays

---
+++ make_messages.js
@@ -0,0 +1,5 @@
+//The following arrays of words will be used to make fun sentences
+const subjects = ['dog', 'hippo', 'plane', 'basketball', 'soda can', 'pencil', 'pumpkin', 'cup'];
+const activeVerbs = ['runs', 'jumps', 'flies', 'cries', 'eats', 'smells', 'walks', 'talks'];
+const prepositions = ['on', 'over', 'around', 'near', 'close to', 'far from', 'under', 'above'];
+const directObjects = ['rock', 'chair', 'table', 'couch', 'stairs', 'stove', 'dishwasher', 'tv'];
