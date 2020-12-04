7c560c34b48fa76d40d462a0996bae5cdad3c9c8
Kane Hall <kanehall27@Kanes-Mac.local>
2020-12-03 19:37:19 -0500

    Commit randIndex function

--- make_messages.js
+++ make_messages.js
@@ -5,10 +5,9 @@ const prepositions = ['on', 'over', 'around', 'near', 'close to', 'far from', 'u
 const directObjects = ['rock', 'chair', 'table', 'couch', 'stairs', 'stove', 'dishwasher', 'tv'];
 
 //The following numbers are randomized for each array each time the program is run.
-let subjectIndex = Math.floor(Math.random() * subjects.length);
-let activeVerbIndex = Math.floor(Math.random() * activeVerbs.length);
-let prepositionIndex = Math.floor(Math.random() * prepositions.length);
-let directObjectIndex = Math.floor(Math.random() * directObjects.length);
+const randIndex = () => {
+    return Math.floor(Math.random() * 8);
+}
 
 //The following prints our weird statement.
-console.log(`The ${subjects[subjectIndex]} ${activeVerbs[activeVerbIndex]} ${prepositions[prepositionIndex]} the ${directObjects[directObjectIndex]}.`);
+console.log(`The ${subjects[randIndex()]} ${activeVerbs[randIndex()]} ${prepositions[randIndex()]} the ${directObjects[randIndex()]}.`);
